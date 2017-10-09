482ce4660046673559f32f76aa1bb199e8e8e80d
student <menase.yonatan@gmail.com>
2017-10-09 11:05:46 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.time.Year;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Name: Yonatan Zewde, and today's date is Oct. 09, 2017");
+
+    }
+}
